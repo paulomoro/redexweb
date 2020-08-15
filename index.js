7e1a7a389ab902792aa418dc7794dc880d48a024
james <jamesakpan016@gmail.com>
2020-08-15 01:00:03 +0100

    firt commit

---
+++ index.js
@@ -0,0 +1,50 @@
+window.onload = () => { 
+  let main = $('main');
+  let form = $('#awaitForm');
+  let year = new Date().getFullYear();
+  let userWidth = screen.width;
+  $(main).fadeOut();
+
+  if(userWidth > 1200 ) { 
+    $('body').css(
+      'background', 'linear-gradient(#ffffff2a, #ffffff2a),url(/Assets/circlePatterns.svg) no-repeat center/120%'
+    )
+  }
+
+  else if(userWidth > 700 ) { 
+    $('body').css(
+      'background', 'linear-gradient(#ffffff2a, #ffffff2a),url(/Assets/circlePatterns.svg) no-repeat top/200%'
+    )
+  }
+  
+  else if(userWidth < 700 ) { 
+    $('body').css(
+      'background', 'linear-gradient(#ffffff2a, #ffffff2a),url(/Assets/circlePatterns.svg) no-repeat top/cover'
+    )
+  }
+
+
+  
+  setInterval(() => { 
+    $(main).fadeIn();
+
+    if(userWidth > 1200 ) { 
+      $('body').css(
+        'background', 'linear-gradient(#fffffff1, #fffffff1),url(/Assets/circlePatterns.svg) no-repeat center/120%'
+      )
+    }
+    else if(userWidth > 700 ) { 
+      $('body').css(
+        'background', 'linear-gradient(#fffffff1, #fffffff1),url(/Assets/circlePatterns.svg) no-repeat top/200%'
+      )
+    }
+    else if(userWidth < 700 ) { 
+      $('body').css(
+        'background', 'linear-gradient(#fffffff1, #fffffff1),url(/Assets/circlePatterns.svg) no-repeat top/cover'
+      )
+    }
+  }, 500) //should be 3000
+
+  //setting footer's year
+  $('#year').text(year);
+}
